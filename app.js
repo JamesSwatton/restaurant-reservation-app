f7196941ff785d044727a88eec1c874014709862
JamesSwatton <jamessswatton@gmail.com>
2020-03-03 19:49:05 +0000

    add required fields and table select

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
     el: '#app',
     data: {
+        tables : {
+            '1': {size: 2},
+            '2': {size: 2},
+            '3': {size: 4},
+            '4': {size: 6},
+            '5': {size: 4},
+            '6': {size: 2},
+        },
         bookings: [],
         newBooking: {
             firstName: '',
@@ -10,6 +18,17 @@ new Vue({
             tableNum: '',
             date: '',
             time: ''
+        }
+    },
+    watch: {
+        "newBooking.partySize": function() {
+            if (this.newBooking.tableNum) {
+                const tableSize = this.tables[this.newBooking.tableNum].size;
+                console.log(tableSize);
+                if (this.newBooking.partySize > tableSize) {
+                    this.newBooking.tableNum = '';
+                }
+            }
         }
     },
     methods: {
